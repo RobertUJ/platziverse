1accb3eefdce52b6cbbb9303b72d1d04f2d105af
Hector Flores <hecto932@gmail.com>
2017-09-17 11:58:38 -0500

    Integracion con el modulo de bases de datos

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -4,11 +4,12 @@ const debug = require('debug')('platziverse:api')
 const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
+const asyncify = require('express-asyncify')
 
 const api = require('./api')
 
 const port = process.env.PORT || 3000
-const app = express()
+const app = asyncify(express())
 const server = http.createServer(app)
 
 app.use('/api', api)
@@ -24,7 +25,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ error: err.message })
 })
 
-function handleFatalError(err) {
+function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
@@ -33,11 +34,10 @@ function handleFatalError(err) {
 if (!module.parent) {
   process.on('uncaughtExeption', handleFatalError)
   process.on('unhandledRejection', handleFatalError)
-  
+
   server.listen(port, () => {
     console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
-  })  
+  })
 }
 
 module.exports = server
-
