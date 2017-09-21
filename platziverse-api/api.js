c97ab4fde85044e6f6afb1db328c54f20f580d37
Hector Flores <hecto932@gmail.com>
2017-09-21 12:20:38 -0500

    Implementando permisos con express-jwt-permissions

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -3,6 +3,7 @@
 const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
 const asyncify = require('express-asyncify')
+const guard = require('express-jwt-permissions')()
 const db = require('platziverse-db')
 const auth = require('express-jwt')
 
@@ -71,7 +72,7 @@ api.get('/agent/:uuid', async (req, res, next) => {
   res.send(agent)
 })
 
-api.get('/metrics/:uuid', async (req, res, next) => {
+api.get('/metrics/:uuid', auth(config.auth), guard.check(['metrics:read']), async (req, res, next) => {
   const { uuid } = req.params
 
   debug(`request to /metrics/${uuid}`)
